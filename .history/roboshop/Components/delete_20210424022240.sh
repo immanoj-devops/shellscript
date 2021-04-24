3c313ce8c2d4b1c7d999b6771a38d7a4805a86c4
mpsre <mp7652@zebra.com>
2021-04-24 02:49:16 -0500

    --- Auto-Git Commit ---

---
+++ .history/roboshop/Components/delete_20210424022240.sh
@@ -0,0 +1,19 @@
+[Unit]
+Description=Student Service 
+After=network.target remote-fs.target nss-lookup.target
+
+
+[Service]
+ExecStart=/home/student/apache-tomcat-8.5.65/bin/startup.sh start   
+ExecReload=/home/student/apache-tomcat-8.5.65/bin/startup.sh start
+ExecStop=home/student/apache-tomcat-8.5.65/bin/shutdown.sh stop 
+# We want systemd to give httpd some time to finish gracefully, but still want
+# it to kill httpd after TimeoutStopSec if something went wrong during the
+# graceful stop. Normally, Systemd sends SIGTERM signal right after the
+# ExecStop, which would kill httpd. We are sending useless SIGCONT here to give
+# httpd time to finish.
+KillSignal=SIGCONT
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
---
+++ .history/roboshop/Components/delete_20210424022347.sh
@@ -0,0 +1,13 @@
+[Unit]
+Description=Student Service 
+After=network.target remote-fs.target nss-lookup.target
+
+
+[Service]
+ExecStart=/home/student/apache-tomcat-8.5.65/bin/startup.sh start   
+ExecReload=/home/student/apache-tomcat-8.5.65/bin/startup.sh restart 
+ExecStop=/home/student/apache-tomcat-8.5.65/bin/shutdown.sh stop 
+Type=forking
+
+[Install]
+WantedBy=multi-user.target
